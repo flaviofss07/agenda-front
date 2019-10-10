ee4033002e526ba831d5a1d60bb493800754e8d7
FLAVIO SOARES DE SOUSA <a9261477@rn.senac.br>
2019-10-09 21:22:33 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ContatoService } from './contato.service';
+
+class Contato {
+  id: number;
+  nome: string;
+  fone: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  ultimoId = 3;
+  contato = new Contato();
+
+  contatos = [];
+
+  constructor(private service: ContatoService) {}
+
+  salvar(formContato: NgForm) {
+
+    /*this.contato.id = ++this.ultimoId;
+    this.contato.nome = formContato.value.nome;
+    this.contato.fone = formContato.value.fone;
+
+    this.contatos.push(this.contato);
+
+    this.contato = new Contato();*/
+
+    console.log(this.service.buscaTodos());
+  }
+
+  editar(id: number) {
+    for (const c of this.contatos) {
+      console.log(c);
+    }
+  }
+
+}
